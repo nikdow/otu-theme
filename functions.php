6d022199995e7ce387fb92af2cb803877f81cabe
cbdweb <nik@cbdweb.net>
2014-10-05 21:44:59 +1100

    custom login logo;

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * login form
+ */
+function my_login_logo() { ?>
+        <style type="text/css">
+            body.login div#login h1 a {
+                background-image: url(/wp-content/uploads/2014/09/otu_logo.png);
+            }
+        </style>
+<?php } 
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
+?>
+ 
