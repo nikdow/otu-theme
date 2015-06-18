8b7bc54015d8ea68e5de0ce23e2b9a811ca2812b
cbdweb <nik@cbdweb.net>
2015-06-18 17:04:34 +1000

    add ngDialog, turn off auto updates;

--- functions.php
+++ functions.php
@@ -16,5 +16,13 @@ function my_login_logo() { ?>
         </style>
 <?php } 
 add_action( 'login_enqueue_scripts', 'my_login_logo' );
-?>
+
+if ( CBDWeb_SUPPRESS_CHECK_UPDATES ) {
+    add_action( 'admin_head', 'suppress_update_checks', 999 );
+}
+function suppress_update_checks() {
+    
+//    remove_action( 'admin_bar_menu', 'stats_admin_bar_menu', 100 );
+    remove_action( 'admin_bar_menu', 'wp_admin_bar_updates_menu', 40 );
+}
  
