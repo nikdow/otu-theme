0408430f16a46212171dc61399a4ee0a7f4d1e98
cbdweb <nik@cbdweb.net>
2017-08-01 22:02:42 +1000

    short code for login form;

--- functions.php
+++ functions.php
@@ -34,4 +34,39 @@ function suppress_update_checks() {
 //    remove_action( 'admin_bar_menu', 'stats_admin_bar_menu', 100 );
     remove_action( 'admin_bar_menu', 'wp_admin_bar_updates_menu', 40 );
 }
+
+add_shortcode('otu_login', 'otu_login' );
+
+function otu_login (  ) {
+    ob_start(); ?>
+    <div class="home_login_wrap">
+        <a name="login"></a>
+        <div class="row">
+            <div class="large-12 columns">
+                 <h3>Login</h3>
+                 <form name="loginform-custom" id="loginform-custom" onSubmit="loginSubmit(this)" action="<?=site_url()?>/wp-login.php" method="post">
+                     <p class="login-username"><label for="user_login">surname</label>
+                         <input type="text" name="partial_log" id="user_login" class="input" size="20"/>
+                         <input type="hidden" name="log" />
+                     </p>
+                     <P class="login-password">
+                         <label for="user_pass">Reg No</label>
+                         <input type="password" name="pwd" id="user_pass" class="input" size="20"/>
+                     </P>
+                     <?php // do_action( 'login_form' );?>
+                     <p class="login-remember">
+                         <label>
+                             <input name="rememberme" type="checkbox" id="rememberme" value="forever">
+                             Remember Me
+                         </label>
+                     </p>
+                     <p class="login-submit">
+                         <input type="submit" name="wp-submit" id="wp-submit" class="button-primary" value="Log In"/>
+                     </p>
+                 </form>
+        </div>
+    </div>
+    <?php
+    return ob_get_clean();
+}
  
